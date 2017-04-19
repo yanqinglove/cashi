939bd20499591ca53a5bae00e53a69559e7cc865
Mac <Mac@macdeimac.local>
2017-04-19 15:48:00 +0800

    xixihah

---
+++ ceshi/ceshi/new.h
@@ -0,0 +1,13 @@
+//
+//  new.h
+//  ceshi
+//
+//  Created by Mac on 17/4/19.
+//  Copyright © 2017年 LiuWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface new : UIView
+
+@end
---
+++ ceshi/ceshi/new.m
@@ -0,0 +1,21 @@
+//
+//  new.m
+//  ceshi
+//
+//  Created by Mac on 17/4/19.
+//  Copyright © 2017年 LiuWei. All rights reserved.
+//
+
+#import "new.h"
+
+@implementation new
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
